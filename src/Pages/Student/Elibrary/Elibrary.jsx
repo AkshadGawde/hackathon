12765380cc3c65f29b96a3faf823f6547bd8a3df
Artilleryxd <karandwari@gmail.com>
2024-03-16 19:47:27 +0530

    AlertPage

--- src/Pages/Student/Elibrary/Elibrary.jsx
+++ src/Pages/Student/Elibrary/Elibrary.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../../../Components/Nav";
 import Footer from "../../../Components/Footer";
+import { Button } from "@material-tailwind/react";
 import {
   Card,
   CardContent,
@@ -28,50 +29,80 @@ const newRequests = [
   },
 ];
 
+const newIssues = [
+  {
+    title: "Vendor 1",
+    description: "Toothpaste bottles have faulty labelling",
+  },
+  {
+    title: "Vendor 2",
+    description: "Leaked Batteries",
+  },
+  {
+    title: "Vendor 3",
+    description: "Pencils broken",
+  },
+  {
+    title: "Vendor 3",
+    description: "Pencils broken",
+  },
+  {
+    title: "Vendor 3",
+    description: "Pencils broken",
+  },
+  {
+    title: "Vendor 3",
+    description: "Pencils broken",
+  }
+];
+
 const Elibrary = () => {
   return (
     <div>
       <Nav />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 m-3">
         <Card>
-            <CardHeader>
-                <CardTitle>Issues</CardTitle>
-            </CardHeader>
-              <CardContent>
-              {newRequests.map((assignment, index) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-between border-b border-gray-200 pb-2"
-                >
-                  <div>
-                    <p className="text-lg font-semibold">{assignment.title}</p>
-                    <p className="text-sm text-gray-500">
-                      {assignment.description}
-                    </p>
-                  </div>
+          <CardHeader>
+            <CardTitle>Issues</CardTitle>
+          </CardHeader>
+          <CardContent className="scrollbar-thin scrollbar-thumb-gray-300 max-h-72 overflow-y-auto">
+            {newIssues.map((assignment, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-between border-b border-gray-200 pb-2"
+              >
+                <div>
+                  <p className="text-lg font-semibold">{assignment.title}</p>
+                  <p className="text-sm text-white-200">
+                    {assignment.description}
+                    <Button color="red" variant="outlined" className="p-1 m-1">
+                      Clear Issue
+                    </Button>
+                  </p>
                 </div>
-              ))}
-              </CardContent>
+              </div>
+            ))}
+          </CardContent>
         </Card>
         <Card>
-            <CardHeader>
-                <CardTitle>Requests</CardTitle>
-            </CardHeader>
-              <CardContent>
-              {newRequests.map((assignment, index) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-between border-b border-gray-200 pb-2"
-                >
-                  <div>
-                    <p className="text-lg font-semibold">{assignment.title}</p>
-                    <p className="text-sm text-gray-500">
-                      {assignment.description}
-                    </p>
-                  </div>
+          <CardHeader>
+            <CardTitle>Requests</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {newRequests.map((assignment, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-between border-b border-gray-200 pb-2"
+              >
+                <div>
+                  <p className="text-lg font-semibold">{assignment.title}</p>
+                  <p className="text-sm text-gray-500">
+                    {assignment.description}
+                  </p>
                 </div>
-              ))}
-              </CardContent>
+              </div>
+            ))}
+          </CardContent>
         </Card>
       </div>
       <Footer />
